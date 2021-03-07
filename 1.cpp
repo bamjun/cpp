9b6a4a1936640021fc9bb071994facdf9963b065
bamjun <khy5116@naver.com>
2021-03-07 16:09:12 +0900

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -23,14 +23,41 @@ int main()
   cout << a_b::score << endl;
 
 
-
-  string phrase = "hoho";
-  cout << phrase.length();
-  cout << phrase[0];
-  //h
-  cout << phrase[1];
-  //o
   cout << "fjie" << endl;
   //한줄 = fjie/n
+  string phrase = "hohoho";
+  cout << phrase.length();
+  cout << endl;
+  cout << phrase[0];
+  //h
+  cout << endl;
+  cout << phrase[1];
+  //o
+  phrase[0] = 'b';
+  cout << endl;
+  cout << phrase << endl;
+  //phrase =  hoho -> boho
+
+  cout << phrase.find("ho", 0) << endl;
+  // 어디서부터 찾을건지, 0 첫글자부터 return2 3 네번째 그자부터. retun 4
+  
+  cout << phrase.substr(1, 3) << endl;
+  // 글자 가져오기 (가져올 위치, 글자수)  oho
+  
+  string a = "abcdefg";
+  string b;
+  b = a.substr(2, 2);
+  cout << b << endl;
+  
+  //////////////////// numbers
+  //////////////////// numbers
+  //////////////////// numbers
+  
+  
+  
+
+
+
+
 
 }
